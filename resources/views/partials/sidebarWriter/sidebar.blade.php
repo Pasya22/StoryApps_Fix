6516355bf06ba271630771c820ae6741ee2b1be7
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2024-03-11 21:01:52 +0700

    UI page writer

---
+++ resources/views/partials/sidebarWriter/sidebar.blade.php
@@ -0,0 +1,10 @@
+<div class="sidebar">
+    <div class="brand" href="">
+      <h3>LOGO</h3>
+    </div>
+    <a href="javascript:void(1)" class="tablink" onclick="openPage('page', this, 'rgb(255, 255, 255)')" id="defaultOpen">DASHBOARD</a>
+    <a href="javascript:void(2)" class="tablink" onclick="openPage('page1', this, 'rgb(255, 255, 255)')">DATA CERITA</a>
+    <a href="javascript:void(3)" class="tablink" onclick="openPage('page2', this, 'rgb(255, 255, 255)')">DATA RATE</a>
+    <a href="javascript:void(5)" class="tablink" onclick="openPage('page3', this, 'rgb(255, 255, 255)')">DATA KOMEN</a>
+    <a href="javascript:void(5)" class="tablink" onclick="openPage('page4', this, 'rgb(255, 255, 255)')">DATA FAVORIT</a>
+  </div>
