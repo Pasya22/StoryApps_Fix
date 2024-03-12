914392188d299380e8cfd4a332de07607fef280d
Pangestu Septiansyah <94443224+Pasya22@users.noreply.github.com>
2024-03-12 16:06:28 +0700

    push

--- resources/views/partials/headerWriter/header.blade.php
+++ resources/views/partials/headerWriter/header.blade.php
@@ -1,55 +1,43 @@
 <div class="nav">
     <div class="nav-area">
-      <div class="nav-burger" onclick="toggleSidebar()">
-        <span class="material-symbols-outlined"> menu </span>
-      </div>
-      <div class="search-box">
-        <input type="text" name="" value="" placeholder="Cari Cerita Favorit Anda" />
-        <button type="submit" name="button">
-          <span class="material-symbols-outlined"> search </span>
-        </button>
-      </div>
-      <div class="nav-menu-dekstop">
-        <div class="notif">
-          <span class="material-symbols-outlined"> notifications </span>
+        <div class="nav-burger" onclick="toggleSidebar()">
+            <span class="material-symbols-outlined"> menu </span>
         </div>
-        <div class="akun">
-          <div class="dropdown">
-            <button onclick="toggleDropdown('dropdown1')" class="dropbtn">
-            <span class="material-symbols-outlined dropbtn ">
-                person
-              </span>
+        <div class="search-box">
+            <input type="text" name="" value="" placeholder="Cari Cerita Favorit Anda" />
+            <button type="submit" name="button">
+                <span class="material-symbols-outlined"> search </span>
             </button>
-            <div id="dropdown1" class="dropdown-content">
-              <div class="akun1">
-                <div class="akun2">
-                  <span class="material-symbols-outlined"> person </span>
-                  Profile
-                </div>
-                <div class="akun2">
-                  <span class="material-symbols-outlined">settings</span>
-                  Settings
-                </div>
-              </div>
-              <div class="akun1">
-                <div class="akun2">
-                  <span class="material-symbols-outlined"> help </span>
-                  Help Center
-                </div>
-                <div class="akun2">
-                  <span class="material-symbols-outlined"> security </span>
-                  Privacy & Safety
-                </div>
-              </div>
-              <div class="akun1">
-                <div class="akun2">
-                  <span class="material-symbols-outlined">login</span>
-                  Log Out
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
+        <div class="nav-menu-dekstop">
+            <div class="notif">
+                <span class="material-symbols-outlined"> notifications </span>
+            </div>
+            <div class="akun">
+                <div class="dropdown">
+                    <button onclick="toggleDropdown('dropdown1')" class="dropbtn">
+                        <span class="material-symbols-outlined dropbtn ">
+                            person
+                        </span>
+                    </button>
+                    <div id="dropdown1" class="dropdown-content">
+                        <div class="akun1">
+                            <div class="akun2">
+                                <span class="material-symbols-outlined"> person </span>
+                                <a href="#">Profile</a>
+                            </div>
+
+                        </div>
+
+                        <div class="akun1">
+                            <div class="akun2">
+                                <span class="material-symbols-outlined">login</span>
+                               <a href="#"> Log Out</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
